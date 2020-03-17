04efa1f19f4b60bfb94232f9bd79b89ce48da244
rithikaprakash <60447231+rithikaprakash@users.noreply.github.com>
2020-03-18 01:55:46 +0530

    Sequence-1,5,13,29,49,77

--- src/Sequence_8.java
+++ src/Sequence_8.java
@@ -9,8 +9,8 @@ public class Sequence_8 {
         n = sc.nextInt();
         for (i=1;i<=n;i+=2)
         {
-            System.out.println(ans_1);
-            System.out.println(ans_2);
+            System.out.print(" "+ans_1+" ");
+            System.out.print(" "+ans_2+" ");
             ans_1=ans_1+(12 * i);
             ans_2=ans_2+(24*j);
             j++;
